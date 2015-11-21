d51f141998b3b3bcbc8dcf0d807ff2dd81bcb834
Jack Thorp <jackthorp13@gmail.com>
2015-11-21 00:25:24 +0000

    Completed coursework question 1

--- Exercise_4/BuildModularNetwork.py
+++ Exercise_4/BuildModularNetwork.py
@@ -9,7 +9,7 @@ from IzNetwork import IzNetwork
 import numpy as np
 import numpy.random as rn
 
-def BuildModularNetwork(M, NPerM, NInh):
+def BuildModularNetwork(M, NPerM, NInh, p):
     """
     Constructs two layers of Izhikevich neurons an connects them together.
     Layer 1 contains M modules of NPerMod number of excitatory neurons.
@@ -17,6 +17,8 @@ def BuildModularNetwork(M, NPerM, NInh):
 
     Inhibitory neurons in layer 2 have diffuse connections to all other inhibitory and excitatory neurons
     in the network.
+
+    p = rewiring probability
 
     """
 
@@ -37,12 +39,23 @@ def BuildModularNetwork(M, NPerM, NInh):
     Dmin = 1    
     Dmax = 20
 
-    net = IzNetwork([NExc, NInh], Dmin)
+    net = IzNetwork([NExc, NInh], Dmax)
     
+    net.layer[EXC].S[EXC] = np.zeros([NExc, NExc])
+    net.layer[EXC].S[INH] = np.zeros([NExc, NInh])
+    net.layer[INH].S[EXC] = np.zeros([NInh, NExc])
+    net.layer[INH].S[INH] = np.zeros([NInh, NInh])
+
     net.layer[INH].factor[INH] = F_INH_INH  
     net.layer[INH].factor[EXC] = F_EXC_INH
     net.layer[EXC].factor[INH] = F_INH_EXC  
     net.layer[EXC].factor[EXC] = F_EXC_EXC
+
+    # Set the conduction delays for connections
+    net.layer[EXC].delay[EXC] = rn.randint(Dmin, Dmax, size=(NExc, NExc))
+    net.layer[EXC].delay[INH] = Dmin * np.ones([NExc, NInh], dtype=int)
+    net.layer[INH].delay[EXC] = Dmin * np.ones([NInh, NExc], dtype=int)
+    net.layer[INH].delay[INH] = Dmin * np.ones([NInh, NInh], dtype=int)
 
     # Excitatory layer (regular spiking)    
     r = rn.rand(NExc)
@@ -63,24 +76,58 @@ def BuildModularNetwork(M, NPerM, NInh):
     # S(i,j) is the strength of the connection from neuron j to neuron i
 
     # EXC -> EXC connections
-    net.layer[EXC].S[EXC] = np.zeros([NExc, NExc])
     for m in range(M):
         c = 0
         while c < 1000:
-            i = randomModuleIndex(m, NPerM)
-            j = randomModuleIndex(m, NPerM)
-            if i != j:
-                #print("({}, {})".format(i,j))
-                net.layer[EXC].S[EXC][i, j] = 1
+            from_neuron = randomModuleIndex(m, NPerM)
+            
+            # Perform probabilistic inter module rewiring
+            rewire = rn.random() < p
+            if rewire:
+                to_module = rn.randint(M)
+                to_neuron = randomModuleIndex(to_module, NPerM)
+            else:
+                to_neuron = randomModuleIndex(m, NPerM)
+
+            # Check not wiring neuron to itself.
+            if rewire or from_neuron != to_neuron:
+                net.layer[EXC].S[EXC][from_neuron, to_neuron] = 1
                 c += 1    
 
-    # EXC -> INH connections
-    #net.layer[1].delay[0]  = D * np.ones([N1, N0], dtype=int)
+    # EXC -> INH connections (connect 4 EXC neurons to one INH and one only)
+    inh_set = range(NInh)
+    # assert NPerM is divisible by exc/inh ratio?
+    for m in range(M):
+        module_set = moduleIndicies(m, NPerM)
+        while len(module_set) > 0: 
+            [inh_neuron, inh_set] = removeRandomNeuron(inh_set)
+            for i in range(4):
+                [exc_neuron, module_set] = removeRandomNeuron(module_set)
+                net.layer[INH].S[EXC][inh_neuron, exc_neuron] = rn.random()
+                
+
+    # INH -> INH & INH -> EXC
+    net.layer[INH].S[INH] = np.ones([NInh, NInh]) - np.identity(NInh)
+    net.layer[EXC].S[INH] = np.ones([NExc, NInh])
+    for i in range(NInh):
+        for j in range(NInh):
+            net.layer[INH].S[INH][i,j] = rn.random() - 1
+        for k in range(NExc):
+            net.layer[EXC].S[INH][k,i] = rn.random() - 1
+
+    #print net.layer[EXC].S[INH]
 
     return net
 
-def randomModuleIndex(M, NPerM):
+def removeRandomNeuron(neuron_set):
+    n = neuron_set.pop(rn.randint(len(neuron_set)))
+    return [n, neuron_set]
+
+def moduleIndicies(m, n_per_m):
+    return range(m*n_per_m, (m*n_per_m + n_per_m))
+
+def randomModuleIndex(m, n_per_m):
     """
     Returns random index in module range (M - M+NPerM)
     """
-    return M*NPerM + int(rn.rand()*NPerM)
+    return rn.choice(moduleIndicies(m, n_per_m), 1)
--- Exercise_4/Run2L.py
+++ Exercise_4/Run2L.py
@@ -10,88 +10,139 @@ consists of 200 inhibitory neurons. Connections are described in the code
 
 from BuildModularNetwork import BuildModularNetwork
 import numpy as np
+import numpy.random as rn
 import matplotlib.pyplot as plt
 from matplotlib.colors import *
-
-T  = 1000  # Simulation time
-Ib = 5    # Base current
+ 
+T   = 1000  # Simulation time
+EXC = 0     # Excitatory layer index
+INH = 1     # Inhibitory layer index
 
 Modules = 8
-ExcitatoryModuleSize = 100
-InhibitoryPoolSize = 200
+NPerM = 100
+NExc = Modules*NPerM
+NInh = 200
 
-net = BuildModularNetwork(Modules, ExcitatoryModuleSize, InhibitoryPoolSize)
-#
-### Initialise layers
-#for lr in xrange(len(net.layer)):
-#  # Set initial voltage to -65 for every neuron in layer 
-#  net.layer[lr].v = -65 * np.ones(net.layer[lr].N)
-#  # Set recovary parameter, u, to ? ? ?
-#  net.layer[lr].u = net.layer[lr].b * net.layer[lr].v
-#  net.layer[lr].firings = np.array([])
-#
-#v1 = np.zeros([T, N1])
-#v2 = np.zeros([T, N2])
-#
-### SIMULATE
-#for t in xrange(T):
-#
-#   # Do Not Need To Inject Current From Here? 
-#   #net.layer[0].I = Ib * np.ones(N1)
-#   #net.layer[1].I = np.zeros(N2)
-#
-#   net.Update(t)
-#
-#   v1[t] = net.layer[0].v
-#   v2[t] = net.layer[1].v
-#
-### Retrieve firings and add Dirac pulses for presentation
-#firings1 = net.layer[0].firings
-##firings2 = net.layer[1].firings
-#
-#if firings1.size != 0:
-#  v1[firings1[:, 0], firings1[:, 1]] = 30
-#
-#if firings2.size != 0:
-#  v2[firings2[:, 0], firings2[:, 1]] = 30
+def main():
+   
+    net = BuildModularNetwork(Modules, NPerM, NInh, 0.3)
 
-# Plot network modules
+    ## Initialise layers
+    for lr in xrange(len(net.layer)):
+      net.layer[lr].v = -65 * np.ones(net.layer[lr].N)
+      net.layer[lr].u = net.layer[lr].b * net.layer[lr].v
+      net.layer[lr].firings = np.array([])
 
-plt.matshow(net.layer[0].S[0], fignum=100, cmap=plt.get_cmap("cool"))
+    v1 = np.zeros([T, NExc])
+    v2 = np.zeros([T, NInh])
 
-## Plot membrane potentials
-#plt.figure(1)
-#plt.subplot(211)
-#plt.plot(range(T), v1)
-#plt.title('Population 1 membrane potentials')
-#plt.ylabel('Voltage (mV)')
-#plt.ylim([-90, 40])
-#
-#plt.subplot(212)
-#plt.plot(range(T), v2)
-#plt.title('Population 2 membrane potentials')
-#plt.ylabel('Voltage (mV)')
-#plt.ylim([-90, 40])
-#plt.xlabel('Time (ms)')
-#
-### Raster plots of firings
-#if firings1.size != 0:
-#  plt.figure(3)
-#  plt.subplot(211)
-#  plt.scatter(firings1[:, 0], firings1[:, 1] + 1, marker='.')
-#  plt.xlim(0, T)
-#  plt.ylabel('Neuron number')
-#  plt.ylim(0, N1+1)
-#  plt.title('Population 1 firings')
+    ## SIMULATE
+    for t in xrange(T):
+        net.layer[INH].I = np.zeros(NInh)
+        net.layer[EXC].I = np.zeros(NExc)
+        # Probabilisitcally add 15mA to membrane potential according to poisson distribution
+        for n in range(NExc):
+            net.layer[EXC].I[n] = 15 if np.random.poisson(0.01) > 0 else 0 
+        
+        net.Update(t)
 
-#if firings2.size != 0:
-#  plt.subplot(212)
-#  plt.scatter(firings2[:, 0], firings2[:, 1] + 1, marker='.')
-#  plt.xlim(0, T)
-#  plt.ylabel('Neuron number')
-#  plt.ylim(0, N2+1)
-#  plt.xlabel('Time (ms)')
-#  plt.title('Population 2 firings')
+        v1[t] = net.layer[EXC].v
+        v2[t] = net.layer[INH].v
 
-plt.show()
+    ## Retrieve firings for presentation
+    firingsExc = net.layer[EXC].firings
+    firingsInh = net.layer[INH].firings
 
+    if firingsExc.size != 0:
+      v1[firingsExc[:, 0], firingsExc[:, 1]] = 30
+
+    if firingsInh.size != 0:
+      v2[firingsInh[:, 0], firingsInh[:, 1]] = 30
+
+    plotFirings(firingsExc)
+    plt.show()
+
+def plotNetworkConnections(net):
+    """
+    Plots connectivity matrices for EXC->EXC and EXC-> INH
+    """
+    plt.figure(1)
+    plt.subplot(11)
+    plt.matshow(net.layer[EXC].S[EXC], fignum=1, cmap=plt.get_cmap("cool"))
+    plt.subplot(22)
+    plt.matshow(net.layer[INH].S[EXC], fignum=2, cmap=plt.get_cmap("cool"))
+    #plt.figure(3)
+    #plt.matshow(net.layer[0].S[1], fignum=3, cmap=plt.get_cmap("cool"))
+    #plt.figure(4)
+    #plt.matshow(net.layer[1].S[1], fignum=4, cmap=plt.get_cmap("cool"))
+
+def plotMembranePotentials():
+    plt.figure(2)
+    plt.subplot(211)
+    plt.plot(range(T), v1)
+    plt.title('Population 1 membrane potentials')
+    plt.ylabel('Voltage (mV)')
+    plt.ylim([-90, 40])
+
+    plt.subplot(212)
+    plt.plot(range(T), v2)
+    plt.title('Population 2 membrane potentials')
+    plt.ylabel('Voltage (mV)')
+    plt.ylim([-90, 40])
+    plt.xlabel('Time (ms)')
+
+def plotInhibitoryFirings(firingsInh):
+    if firingsInh.size != 0:
+        plt.figure(3)
+        plt.scatter(firingsInh[:, 0], firingsInh[:, 1] + 1, marker='.')
+        plt.xlim(0, T)
+        plt.ylabel('Neuron number')
+        plt.ylim(0, NInh+1)
+        plt.xlabel('Time (ms)')
+        plt.title('Population Inh firings')
+
+def plotFirings(firingsExc):
+    """
+    Plots a rasta plot of neuron firigns as well as
+    average firing rates for individual modules.
+    """
+    if firingsExc.size != 0:
+        plt.figure(7)
+        plt.subplot(211)
+        plt.scatter(firingsExc[:, 0], firingsExc[:, 1] + 1, marker='.')
+        plt.xlim(0, T)
+        plt.ylabel('Neuron number')
+        plt.ylim(0, NExc+1)
+        plt.title('Excitatory neuron firings')
+
+    plt.subplot(212)
+    mean_firing = {}
+    for point in firingsExc:
+        [t, idx] = point
+        if t in mean_firing:
+            rest = mean_firing[t]
+            rest.append(idx)
+            mean_firing[t] = rest
+        else:
+            mean_firing[t] = [idx]
+                                              
+    out = np.zeros([8, 50])
+    for j in range(0, T, 20):
+        for k in range(50):
+            if j+k in mean_firing:
+                idxs = mean_firing[j+k]
+                for idx in idxs:
+                    module = idx // 100
+                    out[module][j//20] += 1 
+
+    out2 = out / 50
+    for color in out:
+        t = []
+        t2 = []
+        for i, point in enumerate(color):
+            t.append(i * 20)
+            t2.append(float(point) / 50)
+        plt.plot(t, t2)
+
+if __name__ == '__main__':
+    main()
